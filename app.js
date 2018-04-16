ca909a7206e1420191828b150bf13fb9b47ba9d6
Justin Williams <justin.williams.dhs@gmail.com>
2018-04-16 11:33:39 -0400

    added body-parser library, started to build out the parsing of the board data endpoints credentials

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 let express					= require('express')
 	, app					= express()
 	, config				= require('./config/config.js')
+	, body_parser			= require('body-parser')
 	, format				= require('string-format')
 	, fs					= require('fs')
 	, https					= require('https')
 	, https_port			= config.ports.web_ssl
-	, https_server 			= https.createServer({ cert: fs.readFileSync(config.ssl_certificate), key: fs.readFileSync(config.ssl_key)}, app);
+	, https_server			= https.createServer({ cert: fs.readFileSync(config.ssl_certificate), key: fs.readFileSync(config.ssl_key)}, app);
 
 format.extend(String.prototype);
+
+//	configure app to correctly parse json from body
+app.use(body_parser.json());
+app.use(body_parser.urlencoded({extended: false}));
 
 // allow self-signed certs
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
